c674b5000bbf80e348af0bf529f53e41ceaa727b
hutter <hutteryannick@hotmail.com>
2018-11-09 16:16:21 +0100

    Added basic implementation of website

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $('.timeline').timeline({
+    startIndex: 0,
+    mode: 'vertical'
+  });
+});
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {
+    lat: -25.344,
+    lng: 131.036
+  };
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+    document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
